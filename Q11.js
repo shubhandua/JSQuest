95a50a6519421c7c065647002fdca67a7fa870ba
shubhan <shubhandua@gmail.com>
2025-03-03 23:29:39 +0530

    d

---
+++ Q11.js
@@ -0,0 +1,5 @@
+const favoriteColors = ["Blue", "Red", "Green", "Purple", "Black"];
+
+console.log("First color:", favoriteColors[0]); // Accessing the first element
+console.log("Last color:", favoriteColors[favoriteColors.length - 1]); // Accessing the last element
+
