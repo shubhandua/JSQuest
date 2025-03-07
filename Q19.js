e256b442b8e8791a190b86e1ba62b7da4cce3841
shubhan <shubhandua@gmail.com>
2025-03-07 22:29:55 +0530

    d

---
+++ Q19.js
@@ -0,0 +1,8 @@
+function getArgumentsLength(...args) {
+    return args.length;
+}
+
+// Example usage:
+console.log(getArgumentsLength(1, 2, 3)); // Output: 3
+console.log(getArgumentsLength('a', 'b')); // Output: 2
+console.log(getArgumentsLength()); // Output: 0
