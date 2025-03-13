2315b46627a70fc5359cfd6e2ce3f2466724ce72
shubhan <shubhandua@gmail.com>
2025-03-13 23:57:39 +0530

    d

---
+++ Q40.js
@@ -0,0 +1,16 @@
+function sortArrayDesc(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] < arr[j + 1]) {
+                // Swap the elements
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(sortArrayDesc([5, 3, 8, 1]));
