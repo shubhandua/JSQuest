207fa7476e48001fb8275333ea519092c9540d37
shubhan <shubhandua@gmail.com>
2025-03-15 23:51:54 +0530

    d

--- quote_generator.js
+++ quote_generator.js
@@ -22,7 +22,6 @@
         const quotes = [
             "The only limit to our realization of tomorrow is our doubts of today.",
             "Success is not final, failure is not fatal: It is the courage to continue that counts.",
-            "Believe you can and you're halfway there.",
             "You are never too old to set another goal or to dream a new dream.",
             "Success usually comes to those who are too busy to be looking for it."
         ];
