e68ce00100245ade0ff1a60d97475a035d46fe3c
shubhan <shubhandua@gmail.com>
2025-04-08 23:52:51 +0530

    Heap sort

---
+++ Q109.js
@@ -0,0 +1,27 @@
+function heapSort(arr) {
+    let n = arr.length;
+
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) heapify(arr, n, i);
+    for (let i = n - 1; i > 0; i--) {
+        [arr[0], arr[i]] = [arr[i], arr[0]];
+        heapify(arr, i, 0);
+    }
+
+    return arr;
+}
+
+function heapify(arr, n, i) {
+    let largest = i;
+    let l = 2 * i + 1;
+    let r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest]) largest = l;
+    if (r < n && arr[r] > arr[largest]) largest = r;
+
+    if (largest !== i) {
+        [arr[i], arr[largest]] = [arr[largest], arr[i]];
+        heapify(arr, n, largest);
+    }
+}
+
+console.log(heapSort([10, 3, 5, 1, 9, 7, 2, 6, 4, 8]));
