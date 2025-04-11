8037b7673836a119c4b5ff3b3d29467e3b826499
shubhan <shubhandua@gmail.com>
2025-04-11 23:45:43 +0530

    Rectangle Class with Area and Perimeter

---
+++ Q118.js
@@ -0,0 +1,18 @@
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    area() {
+        return this.width * this.height;
+    }
+
+    perimeter() {
+        return 2 * (this.width + this.height);
+    }
+}
+
+const rect = new Rectangle(5, 3);
+console.log(rect.area());
+console.log(rect.perimeter());
