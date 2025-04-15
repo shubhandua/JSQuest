834c1d40573de871d6dc178b1a8e86b17f77821d
shubhan <shubhandua@gmail.com>
2025-04-15 23:28:41 +0530

    Shape, Circle, and Triangle Classes with Area Calculation

---
+++ Q122.js
@@ -0,0 +1,34 @@
+class Shape {
+    area() {
+        return 0;
+    }
+}
+
+class Circle extends Shape {
+    constructor(radius) {
+        super();
+        this.radius = radius;
+    }
+
+    area() {
+        return Math.PI * this.radius ** 2;
+    }
+}
+
+class Triangle extends Shape {
+    constructor(base, height) {
+        super();
+        this.base = base;
+        this.height = height;
+    }
+
+    area() {
+        return 0.5 * this.base * this.height;
+    }
+}
+
+const circle = new Circle(5);
+const triangle = new Triangle(10, 6);
+
+console.log(circle.area());
+console.log(triangle.area());
