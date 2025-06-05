d80dd8b19e9adf57267aad786d86fea63ac021c3
shubhan <shubhandua@gmail.com>
2025-06-05 22:19:33 +0530

    Dropdown Menu Toggle

---
+++ Q150.js
@@ -0,0 +1,4 @@
+document.getElementById("dropdownButton").addEventListener("click", function() {
+  const menu = document.getElementById("dropdownMenu");
+  menu.style.display = menu.style.display === "none" ? "block" : "none";
+});
